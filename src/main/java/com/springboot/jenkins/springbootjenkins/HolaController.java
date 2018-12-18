adbc82f7c710fa5976cca8dbf59ba342d4164253
Mark <mguzman@nisum.com>
2018-12-18 16:52:00 -0300

    update

--- src/main/java/com/springboot/jenkins/springbootjenkins/HolaController.java
+++ src/main/java/com/springboot/jenkins/springbootjenkins/HolaController.java
@@ -1,4 +1,13 @@
 package com.springboot.jenkins.springbootjenkins;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HolaController {
+
+    @GetMapping(path = "holaMundo")
+    public String holaMundo(){
+        return "Hola mundo";
+    }
 }
